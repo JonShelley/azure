23cffb1a06be944491aa695e2f30321d9db36a2c
Jon Shelley <JonShelley@users.noreply.github.com>
2021-11-09 15:26:50 -0700

    Update nccl-test-build.sh

--- benchmarking/NDv4/cc-slurm-ngc/nccl/scripts/nccl-test-build.sh
+++ benchmarking/NDv4/cc-slurm-ngc/nccl/scripts/nccl-test-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   
 CONT="nvcr.io#nvidia/pytorch:20.10-py3"
-MOUNT="/shared/data/azure/benchmarking/NDv4/cc-slurm-ngc/nccl:/nccl,/opt/hpcx-v2.8.3-gcc-MLNX_OFED_LINUX-5.2-2.2.3.0-ubuntu18.04-x86_64:/opt/hpcx"
+MOUNT="/shared/data/azure/benchmarking/NDv4/cc-slurm-ngc/nccl:/nccl,/opt/hpcx-v2.9.0-gcc-MLNX_OFED_LINUX-5.4-1.0.3.0-ubuntu18.04-x86_64:/opt/hpcx"
 
 export OMPI_MCA_pml=ucx
 export OMPI_MCA_btl=^openib
