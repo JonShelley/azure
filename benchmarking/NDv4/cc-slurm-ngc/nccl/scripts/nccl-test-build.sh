b5142aa3448045b8cafd92c954b04f289eb45c09
sdoomra <104459900+sdoomra@users.noreply.github.com>
2022-09-27 11:53:54 -0700

    Update nccl-test-build.sh
    Update hpc version with ubuntu 20.04

--- benchmarking/NDv4/cc-slurm-ngc/nccl/scripts/nccl-test-build.sh
+++ benchmarking/NDv4/cc-slurm-ngc/nccl/scripts/nccl-test-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   
 CONT="nvcr.io#nvidia/pytorch:21.09-py3"
-MOUNT="/shared/data/azure/benchmarking/NDv4/cc-slurm-ngc/nccl:/nccl,/opt/hpcx-v2.9.0-gcc-MLNX_OFED_LINUX-5.4-1.0.3.0-ubuntu18.04-x86_64:/opt/hpcx"
+MOUNT="/shared/data/azure/benchmarking/NDv4/cc-slurm-ngc/nccl:/nccl,/opt/hpcx-v2.9.0-gcc-MLNX_OFED_LINUX-5.4-1.0.3.0-ubuntu20.04-x86_64:/opt/hpcx"
 
 export OMPI_MCA_pml=ucx
 export OMPI_MCA_btl=^openib
